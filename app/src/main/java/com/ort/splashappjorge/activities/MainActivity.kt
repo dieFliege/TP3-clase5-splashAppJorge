13a297e4401914ce528c0a86c67cc4fa1c22805d
jorge-alonso <jorge.a.alonso91@gmail.com>
2022-04-29 18:43:29 -0300

    práctica completa

---
+++ app/src/main/java/com/ort/splashappjorge/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.ort.splashappjorge.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ort.splashappjorge.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var pepe : String
+    }
+}
